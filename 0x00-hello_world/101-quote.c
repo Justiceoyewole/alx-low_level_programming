689011f10277a5b1340884f69c726356e2159980
root <justiceoyewo@gmail.com>
2024-05-15 14:27:51 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - this prints exactly the input on it
+ * Return: always 1
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+return (1);
+}
