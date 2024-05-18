fdb7351de5b6672953f73e0673f734bbd8d993fa
root <justiceoyewo@gmail.com>
2024-05-18 11:08:22 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - thsi is main function
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+
+int d;
+char low;
+
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+
+for (low = 'a'; low <= '9'; low++)
+putchar(low);
+putchar('\n');
+
+return (0);
+}
