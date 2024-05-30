588fea1ebd1ebc9fa608c4d15900cd32cca9fc69
root <justiceoyewo@gmail.com>
2024-05-30 11:38:22 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,15 +4,14 @@
  * main - test function that prints if integer is positive or negative
  * Return: 0
  */
-int main(void)
 
+int main(void)
 {
 
 int i;
 
 i = 0;
 positive_or_negative(1);
-
 return (0);
 
 }
